08ce5eabb15b3b3ac2ea803ae85a91a14f4ed1ab
hasanhaja <hasanhaja@gmail.com>
2019-08-31 13:05:44 +0100

    Simple stat analysis implemented

---
+++ README.md
@@ -0,0 +1,9 @@
+[![forthebadge made-with-python](http://ForTheBadge.com/images/badges/made-with-python.svg)](https://www.python.org/)
+# CSV Analyzer
+This project is an exercise to learn Python.
+
+# Usage
+Clone the repo and run it using `python analyzer.py <file_name>`.
+
+# Maintenance
+Since this is an academic project, I only intend on maintaining and adding functionality to experiment with new concepts. Otherwise, this project won't be actively maintained.
--- analyzer.py
+++ analyzer.py
@@ -1,13 +1,51 @@
 import csv
+import statistics as stats
 
 def extract_value(array):
     array_tokens = array[0].split(";")
     value = float(array_tokens[1])
     return value
 
+def process(list):
+    """
+    This function does all the mathematical heavy lifting and returns the data as a dictionary (aka map).
+    """
+    mean = stats.mean(list)
+    #Error checking for mode
+    try:
+        mode = stats.mode(list)
+    except:
+        mode = "N/A"
+    
+    median = stats.median(list)
+    stdev = stats.stdev(list)
+    variance = stats.variance(list)
+    
+    results = {
+        "mean": str(mean),
+        "mode": str(mode),
+        "median": str(median),
+        "stdev": str(stdev),
+        "variance": str(variance),
+    }
 
-def average_values(list):
-    return sum(list) / len(list)
+    if results["mode"] == "N/A":
+        results.pop("mode")
+        return results
+
+    return results
+
+def stringify_results(results):    
+    text = ""
+    for result_key in results:
+        text += result_key.title() + ": " + results[result_key] + "\n"
+    return text
+
+def analyze(list):
+    """
+    This formats the data from the process function and outputs only the relavant data.
+    """
+    return stringify_results(process(list))
 
 def main():
     with open('price_history_LU1664415285.csv') as csv_file:
@@ -23,9 +61,8 @@ def main():
             else:
                 values.append(extract_value(row))
 
-        average = average_values(values)
-
-        print(average)
+        result = analyze(values)
+        print(result)
 
 if __name__ == '__main__':
     main()
